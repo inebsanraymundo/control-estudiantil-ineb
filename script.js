8b865997dc1df2be556aa868fbb5e0944217cffb
INEB San Raymundo <inebsanraymundo1@gmail.com>
2026-04-10 10:00:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API="https://script.google.com/macros/s/AKfycbxyVWhRwxBu3xuNkGLGayoGLlT5nYaqy1ohhKT5lqrrv_79E3ReTYA-W97YYu-c9gi7/exec"
+const API="https://script.google.com/macros/s/AKfycbw49zVrVtcNhglYTNqTMZKDPSBq6gkf7yspB5n14Vo9LkHKCQwJaXZPmEEJO4XAUzjd/exec"
 
 function getData(){
 
