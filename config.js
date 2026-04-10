606f9779a5bf5eccb9f8d9ec939c0a9e344a7512
INEB San Raymundo <inebsanraymundo1@gmail.com>
2026-04-10 10:16:25 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw49zVrVtcNhglYTNqTMZKDPSBq6gkf7yspB5n14Vo9LkHKCQwJaXZPmEEJO4XAUzjd/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzJvctWaW7MYT5Iz6ZfsrDi4L9Z0mZj7XQpDb-jsCrXuX8aB6Y1zjZ-S28VbVjv6_zF/exec";
 let USER_ROLE="usuario"
